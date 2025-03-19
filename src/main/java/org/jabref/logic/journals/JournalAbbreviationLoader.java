6d8fad7c8e0b866429c1dad217b3f4b60906d362
Siedlerchr <siedlerkiller@gmail.com>
2025-03-19 21:03:21 +0100

    ceckstyle

--- src/main/java/org/jabref/logic/journals/JournalAbbreviationLoader.java
+++ src/main/java/org/jabref/logic/journals/JournalAbbreviationLoader.java
@@ -3,8 +3,8 @@ package org.jabref.logic.journals;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
