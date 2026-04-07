1856f1fcce6e1a333e7089693ad045bf513e4cd4
Siedlerchr <siedlerkiller@gmail.com>
2026-04-07 22:11:19 +0200

    checkstyle

--- jablib/src/test/java/org/jabref/logic/preferences/JabRefGuiPreferencesTest.java
+++ jablib/src/test/java/org/jabref/logic/preferences/JabRefGuiPreferencesTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class JabRefGuiPreferencesTest {
     private static Stream<Arguments> provideTestData() {
